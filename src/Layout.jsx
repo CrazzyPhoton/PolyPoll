f6a0b250c7610133f7c179cb6d7b3d4787dfca88
CrazzyPhoton <mayekarrahul26@gmail.com>
2025-09-01 19:00:44 +0530

    Add Footer component to Layout and update structure

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import { ScrollRestoration, Outlet } from 'react-router-dom'
 import { Navbar } from './components/Navbar.jsx'
+import { Footer } from './components/Footer.jsx'
 
 export function Layout() {
   return (
-    <>
+    <div className="bg-info-subtle">
       <Navbar />
       <Outlet />
+      <Footer />
       <ScrollRestoration />
-    </>
+    </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <div className="container-fluid d-flex align-items-center justify-content-center bg-dark py-3 mt-5">
+            <p className="text-white fw-bold text-break mt-3">© 2025 Polypoll. All rights reserved.</p>
+        </div>
+    )
+}
