3a85655dd05faf1a5119025c61ba77ffb2c3bed5
CrazzyPhoton <mayekarrahul26@gmail.com>
2025-09-02 15:45:30 +0530

    Rename project from "pollapp" to "polypoll" and update favicon colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export function Footer() {
     return (
         <div className="container-fluid d-flex align-items-center justify-content-center bg-dark py-3 mt-5">
-            <img src="/polypoll_favicon.svg" alt="Polypoll Logo" width="32" height="32" className="me-2" />
-            <p className="text-white fw-bold fs-4 text-break mt-3">Polypoll</p>
+            <img src="/polypoll_favicon.svg" alt="PolyPoll Logo" width="32" height="32" className="me-2" />
+            <p className="text-white fw-bold fs-4 text-break mt-3">PolyPoll</p>
         </div>
     )
 }
